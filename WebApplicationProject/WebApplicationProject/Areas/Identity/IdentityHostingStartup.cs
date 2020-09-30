a1f3c7f5bee6e494b9d1680e4e7c3ad5a8648679
Jonathan St-Amant <jtnstamant@gmail.com>
2020-09-30 18:15:58 -0400

    Initial commit

---
+++ WebApplicationProject/WebApplicationProject/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebApplicationProjectData;
+
+[assembly: HostingStartup(typeof(WebApplicationProjectUI.Areas.Identity.IdentityHostingStartup))]
+namespace WebApplicationProjectUI.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<WebApplicationProjectContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("WebApplicationProjectContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<WebApplicationProjectContext>();
+            });
+        }
+    }
+}
